980d49dae54b3fa86b50028a47bc8ef6ee6096a4
Dieguet <36648248+DiegoHilderink@users.noreply.github.com>
2020-05-08 11:20:04 +0200

    otro deploy

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist/rentel-bot-conf'));
+app.use(express.static('./dist/rentel-bot'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/rentel-bot-conf/'}),
+    res.sendFile('index.html', {root: 'dist/rentel-bot/'}),
 );
 
 app.listen(process.env.PORT || 8080);
